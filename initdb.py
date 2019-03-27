bfd576372d967e015dbbcc8932bb574c085c1131
Aethyn <mmi.theleme@gmail.com>
2019-03-27 12:06:15 +0100

    Adding comments, data are recorded in base

--- initdb.py
+++ initdb.py
@@ -1,6 +1,10 @@
+# coding:utf-8
 import psycopg2
 import env
 
+# Ce fichier sert à initialiser la base de données
+
+# Les données de connexion à la base
 relationnal_connection_data = {
     "dbname": env.dbname,
     "user": env.user,
@@ -9,33 +13,36 @@ relationnal_connection_data = {
     "port": env.port
 }
 
+# On se connecte à la base et on récupère un curseur (pour executer des instructions)
 with psycopg2.connect(**relationnal_connection_data) as connection:
+    # Si les tables line et stop existent, on les delete (clear database pour insertion de nouvelles données)
     with connection.cursor() as cursor:
         cursor.execute("""
-        DROP TABLE IF EXISTS line CASCADE
+            DROP TABLE IF EXISTS line CASCADE
         """)
         cursor.execute("""
-        DROP TABLE IF EXISTS stop CASCADE
+            DROP TABLE IF EXISTS stop CASCADE
         """)
     
+    # Création de la table line qui va stocker les lignes de transports et stop qui va stocker les arrêts
     with connection.cursor() as cursor:
         cursor.execute("""
-        CREATE TABLE IF NOT EXISTS line (
-            id VARCHAR(256) PRIMARY KEY,
-            name VARCHAR(256)
-        );
+            CREATE TABLE IF NOT EXISTS line (
+                id VARCHAR(256) PRIMARY KEY,
+                name VARCHAR(256)
+            );
         """)
-
         cursor.execute("""
-        CREATE TABLE IF NOT EXISTS stop (
-            id VARCHAR(256) PRIMARY KEY,
-            name VARCHAR(256),
-            line_id VARCHAR(256) REFERENCES line (id),
-            lon REAL,
-            lat REAL
-        );
+            CREATE TABLE IF NOT EXISTS stop (
+                id VARCHAR(256) PRIMARY KEY,
+                name VARCHAR(256),
+                line_id VARCHAR(256) REFERENCES line (id),
+                lon REAL,
+                lat REAL
+            );
         """)
-
+        
+        # On print dans la console les tables existantes pour vérifier qu'elles ont bien été insérées
         cursor.execute("SELECT * FROM pg_catalog.pg_tables;")
         rows = cursor.fetchall()
         print("tables", "line" in [r[1] for r in rows])
--- irigo.py
+++ irigo.py
@@ -1,7 +1,10 @@
+# coding:utf-8
 import bonobo
 import requests
+import psycopg2
 import env
 
+# Les données de connexion à la base
 relationnal_connection_data = {
     "dbname": env.dbname,
     "user": env.user,
@@ -10,12 +13,14 @@ relationnal_connection_data = {
     "port": env.port
 }
 
+# Fonction utilisant un générateur qui appelle l'url de l'API et retourne les résultats (records)
 def fetch_desserte_data():
     url = "https://data.angers.fr/api/records/1.0/search/?dataset=bus-tram-topologie-dessertes&"
     response = requests.get(url)
     for record in response.json().get("records"):
         yield record
 
+# Fonction utilisant un générateur qui retourne uniquement les champs intéressants parmi les données récupérées
 def get_interesting_fields(record):
     result = {
         "line_id": record["fields"]["mnemoligne"],
@@ -24,18 +29,20 @@ def get_interesting_fields(record):
         "stop_name": record["fields"]["nomarret"]
     }
     yield result
-        
+
+# Fonction utilisant un générateur qui se connecte à la base et tente d'enregistrer les données dans la table des lignes
 def insert_in_relationnal(record):
     with psycopg2.connect(**relationnal_connection_data) as connection:
         with connection.cursor() as cursor:
             try:
                 cursor.execute("""
-                INSERT INTO line (id, name) VALUES (%s, %s);
+                    INSERT INTO line (id, name) VALUES (%s, %s);
                 """, (record["line_id"], record["line_name"]))
             except psycopg2.IntegrityError as e:
                 print("line already exists", record["line_id"])
     yield record
-    
+
+# Génération du Graph bonobo
 graph = bonobo.Graph()
 graph.add_chain(
     fetch_desserte_data, 
@@ -43,5 +50,5 @@ graph.add_chain(
     insert_in_relationnal,
     bonobo.PrettyPrinter()
 )
-graph.add_chain(g, _input=fetch_desserte_data)
+graph.add_chain(graph, _input=fetch_desserte_data)
 r = bonobo.run(graph)
